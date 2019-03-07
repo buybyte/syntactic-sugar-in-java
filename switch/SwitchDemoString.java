2cdef884386379b6cfbf873c588572cadc2b8e25
stardust <aoyunyoung@gmail.com>
2019-03-07 16:25:00 +0800

    add string switch

---
+++ switch/SwitchDemoString.java
@@ -0,0 +1,54 @@
+public class switchDemoString {
+    public static void main(String[] args) {
+        String str = "world";
+        switch (str) {
+        case "hello":
+            System.out.println("hello");
+            break;
+        case "world":
+            System.out.println("world");
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+/**
+ java -jar cfr-0.140.jar SwitchDemoString.class --decodestringswitch false
+/*
+ * Decompiled with CFR 0.140.
+ 
+import java.io.PrintStream;
+
+public class SwitchDemoString {
+    public static void main(String[] arrstring) {
+        String string;
+        String string2 = string = "world";
+        int n = -1;
+        switch (string2.hashCode()) {
+            case 99162322: {
+                if (!string2.equals("hello")) break;
+                n = 0;
+                break;
+            }
+            case 113318802: {
+                if (!string2.equals("world")) break;
+                n = 1;
+            }
+        }
+        switch (n) {
+            case 0: {
+                System.out.println("hello");
+                break;
+            }
+            case 1: {
+                System.out.println("world");
+                break;
+            }
+        }
+    }
+}
+
+
+**/
