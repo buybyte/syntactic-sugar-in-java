b204e78b42682a3c0e552953fc9820e2bb4a8e1a
stardust <aoyunyoung@gmail.com>
2019-03-07 16:06:56 +0800

    refactor

--- try-with-resource/TryWithResource.java
+++ try-with-resource/TryWithResource.java
@@ -2,20 +2,63 @@ import java.io.*;
 import java.lang.Exception;
 class TryWithResource{
     public static void main(String[] args) {
-        FileInputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(new File("test"));
+        try (FileInputStream inputStream = new FileInputStream(new File("test"))) {
             System.out.println(inputStream.read());
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage(), e);
-        } finally {
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e.getMessage(), e);
-                }
-            }
         }
     }
     }
+	
+/**
+D:\>java -jar cfr-0.140.jar TryWithResource.class --sugarboxing false
+/*
+ * Decompiled with CFR 0.140.
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+class TryWithResource {
+    TryWithResource() {
+    }
+
+    public static void main(String[] arrstring) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream((File)new File((String)"test"));
+            Throwable throwable = null;
+            try {
+                System.out.println((int)fileInputStream.read());
+            }
+            catch (Throwable throwable2) {
+                throwable = throwable2;
+                throw throwable2;
+            }
+            finally {
+                if (fileInputStream != null) {
+                    if (throwable != null) {
+                        try {
+                            fileInputStream.close();
+                        }
+                        catch (Throwable throwable3) {
+                            throwable.addSuppressed((Throwable)throwable3);
+                        }
+                    } else {
+                        fileInputStream.close();
+                    }
+                }
+            }
+        }
+        catch (IOException iOException) {
+            throw new RuntimeException((String)iOException.getMessage(), (Throwable)iOException);
+        }
+    }
+}
+
+
+
+
+
+
+**/
